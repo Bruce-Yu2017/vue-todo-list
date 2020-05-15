3918e169e70a92dc24ad30a90f6cba111cde3756
Jiahao Yu <ilywanzi@gmail.com>
2020-05-14 23:47:59 -0700

    vue todo list

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit="addTodo">
+    <input type="text" placeholder="在这里填写任务" v-model="todo">
+    <input type="submit" value="提交" class="btn">
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AddTodo',
+  data() {
+    return {
+      todo: ''
+    }
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const data = {
+        title: this.todo,
+        completed: false
+      }
+      this.$emit('add-todo', data);
+      this.todo = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-completed': todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" v-model="todo.completed">
+      {{todo.title}}
+      <button class="del" v-on:click="$emit('del-todo', todo.id)" >x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete(e) {
+      this.todo.completed = e.target.checked;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  border-bottom: 1px #ccc dotted;
+  padding: 10px;
+}
+
+.is-completed {
+  text-decoration: line-through;
+}
+
+.del {
+  background: #d15d5d;
+  color: #000;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
